3c2445d6f414a04973de0ae4297abc36bbe51d82
Inoue Shinichi Macbook Pro 2016 <inoue.shinichi.1800@gmail.com>
2023-06-16 18:41:50 +0900

    [Update] ch04-flask-app2:v1

--- Ch04_Dockerfile/docker/flask_server.py
+++ Ch04_Dockerfile/docker/flask_server.py
@@ -6,14 +6,17 @@ import os
 # 3rdParty
 import flask
 
-HOST = os.environ['HOST']
 PORT = int(os.environ['PORT'])
 app = flask.Flask('app server')
 
 @app.route('/')
-def index():
+def route_index():
     return 'hello Dockerfile'
 
+@app.route('/api/v1/hello')
+def api_index():
+    return 'hello api v1 Dockerfile'
+
 # run application 
-app.run(debug=True, host=HOST, port=PORT)
+app.run(debug=True, host="0.0.0.0", port=PORT)
 
