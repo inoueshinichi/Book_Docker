de619cda2735db61ff404b1e935a9f34effa2946
Inoue Shinichi Macbook Pro 2016 <inoue.shinichi.1800@gmail.com>
2023-06-29 19:10:56 +0900

    [Update] proceed

---
+++ Ch05_DockerCompose/webappdb_3layers/app/bind_src/flask_server.py
@@ -0,0 +1,127 @@
+"""FlaskによるDB用REST_APIサーバ
+"""
+import os
+import re
+import redis
+from flask import (
+    Flask,
+    jsonify,
+    request,
+)
+
+# Redis db conf
+REDIS_HOST = os.environ['REDIS_HOST']
+REDIS_PORT = os.environ['REDIS_PORT']
+REDIS_DB = os.environ['REDIS_DB']
+
+# Redis interface
+REDIS = redis.Redis(host=REDIS_HOST, 
+                    port=REDIS_PORT,
+                    db=REDIS_DB,
+                    )
+
+# App port (for this REST_API server)
+APP_PORT = int(os.environ['APP_PORT'])
+
+# REST_API server
+app = Flask("rest-api app server")
+
+API_VER = 'v1'
+DB_STORE_COUNT = 1000000 # 100万
+
+@app.route(f'/api/{API_VER}/keys/', methods=['GET'])
+def api_keys():
+    data = {}
+    cursor = '0'
+    while cursor != 0:
+        cursor, keys = REDIS.scan(cursor=cursor,
+                                  count=DB_STORE_COUNT,
+                                  )
+        if len(keys) == 0:
+            break
+        keys = [key.decode() for key in keys]
+        values = [value.decode() for value in REDIS.mget(*keys)]
+        data.update(dict(zip(keys, values)))
+    
+    return success(data)
+
+@app.route(f'/api/{API_VER}/keys/<key>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def api_key(key):
+    if not isalnum(key):
+        return error(400.1)
+    
+    body = request.get_data().decode().strip()
+    if request.method in ['POST', 'PUT']:
+        if body == '':
+            return error(400.2)
+        if not isalnum(body):
+            return error(400.3)
+    
+    def get():
+        value = REDIS.get(key)
+        if value is not None: # valueが空
+            return success({key:value.decode()})
+        return error(404)
+    
+    def post():
+        if REDIS.get(key) is not None: # 既にkeyが存在
+            return error(409)
+        REDIS.set(key, body)
+        return success({key:body})
+    
+    def put():
+        REDIS.set(key, body)
+        return success({key:body})
+    
+    def delete():
+        if REDIS.delete(key) == 0: # keyが空
+            return error(404)
+        return success({})
+    
+    func_dict = {
+        'GET': get,
+        'POST': post,
+        'PUT': put,
+        'DELETE': delete,
+    }
+    return func_dict[request.method]() # HTTPのAPIに応じてRedisとの仲介を行う
+
+def isalnum(text):
+    # 半角英数字で構成された文字列にマッチした場合, Trueを返す
+    return re.match(r'^[a-zA-Z0-9]+$', text) is not None
+    
+def success(kv):
+    # ブラウザにJson形式で値を返す
+    return jsonify(kv), 200 # HTTPレスポンスコード 200 : 成功
+
+def error(code):
+    message = {
+        400.1: "bad request. key must be alnum",
+        400.2: "bad request. post/put needs value on body",
+        400.3: "bad request. value must be alnum",
+        404: "request not found",
+        409: "resource conflict. resource already exist",
+    }
+    return jsonify({'error': message[code], 'code': int(code)}), int(code)
+
+# 404 レスポンスコード(アクセスデータが存在しない) ハンドラ
+@app.errorhandler(404)
+def api_not_found_error(error):
+    return jsonify({'error': "api not found", 'code': 404}), 404
+
+# 405 レスポンスコード(アクセス権限) ハンドラ
+@app.errorhandler(405)
+def method_not_allowed_error(error):
+    return jsonify({'error': "method not allowed", 'code': 405}), 405
+
+# 500 レスポンスコード(サーバー内部エラー) ハンドラ
+@app.errorhandler(500)
+def internal_server_error(error):
+    return jsonify({'error': 'server internal error', 'code': 500}), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True,
+            host='0.0.0.0',
+            port=APP_PORT,
+            )
--- Ch05_DockerCompose/webappdb_3layers/web/start.sh
+++ Ch05_DockerCompose/webappdb_3layers/web/start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# NginxのWebサーバ設定からリバースプロキシーサーバ設定への変更
+sed -e "s/{{NAIVE_HTTP_PORT}}/$NAIVE_HTTP_PORT/g" /etc/nginx/nginx.conf.tmp > /etc/nginx/nginx.conf
+sed -i -e "s^{{APP_SERVER}}^$APP_SERVER^g" /etc/nginx/nginx.conf
 
-# ここに, PORTの変換 in nginx.conf.tmp
+# PID1としてNginxを起動する
 exec nginx -g "daemon off;"
